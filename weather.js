ca6cdd81adc7fe1d4d9e3edc0c37e57b8c6387d9
vaibhav9133 <vaibhavsingh411@gmail.com>
2023-04-07 01:41:30 +0530

    Updated 1

--- weather.js
+++ weather.js
@@ -1,9 +1,9 @@
 const express = require("express")
-const request = require("request")
 const http = require("https")
 const bodyParser = require("body-parser")
 
 const app = express()
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', function(req, res) {
 
@@ -16,8 +16,8 @@ app.post("/", function(req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=f5183ca35c3bfbdd1d2ebb11a2a56962"
 
     http.get(url, function(response) {
-        response.on("data", function(da) {
-            const WeatherData = JSON.parse(da)
+        response.on("data", function(data) {
+            const WeatherData = JSON.parse(data)
             let temp = WeatherData.main.temp
             const icon = WeatherData.weather[0].icon
 
@@ -25,11 +25,14 @@ app.post("/", function(req, res) {
             let t = Number.parseInt(temp)
             console.log(t)
             console.log(desc)
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-            res.write("hi")
-            res.send(`The temperature of ${query } is ${t}`)
 
+            const ic = WeatherData.weather[0].icon
+            const img = "http://openweathermap.org/img/w/" + ic + ".png";
 
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius.</h1>")
+            res.write("<p>The weather is currently " + desc + ".</p>")
+            res.write("<img src='" + img + "' alt='Weather icon'>")
+            res.send()
         })
 
 
